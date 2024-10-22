c2a940156404060cd28d02692097285ccffd64db
Akn <adni@itu.dk>
2024-10-22 15:26:53 +0200

    update

--- server/server.go
+++ server/server.go
@@ -5,31 +5,36 @@ import (
 	"log"
 	"net"
 	"strconv"
+	"time"
 
 	pb "DS-handin3/service/github.com/BirdyDK/DS-handin3"
 
 	"google.golang.org/grpc"
 )
 
-type server struct{}
+type server struct {
+	pb.UnimplementedChittyChatServer
+}
+
+var timestamp int32 = 0
+
+func UpdateTimestamp(newTime int32) int32 {
+	if newTime > timestamp {
+		timestamp = newTime
+	}
+	timestamp++
+	return timestamp
+}
 
 func (s *server) Broadcast(ctx context.Context, in *pb.BroadcastRequest) (*pb.BroadcastResponse, error) {
 	return &pb.BroadcastResponse{Message: in.Message + strconv.Itoa(int(in.Timestamp))}, nil
 }
 
-func (s *server) Publish(ctx context.Context, in *pb.PublishRequest) (*pb.PublishResponse, error) {
-	return &pb.PublishResponse{Message: in.Name + in.Message}, nil
-}
-
 func (s *server) Join(ctx context.Context, in *pb.JoinRequest) (*pb.JoinResponse, error) {
-	// Broadcast join message to all participants
-	// Implement logical clock logic if needed
 	return &pb.JoinResponse{Message: "Participant " + in.Name + " joined Chitty-Chat"}, nil
 }
 
 func (s *server) Leave(ctx context.Context, in *pb.LeaveRequest) (*pb.LeaveResponse, error) {
-	// Broadcast leave message to all participants
-	// Implement logical clock logic if needed
 	return &pb.LeaveResponse{Message: "Participant " + in.Name + " left Chitty-Chat"}, nil
 }
 
@@ -42,5 +47,9 @@ func main() {
 	pb.RegisterChittyChatServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+	for {
+
+		time.Sleep(time.Second)
 	}
 }
